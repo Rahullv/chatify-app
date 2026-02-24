f25726f3145977211e6dc9292d762c7c9f2806ac
Rahul Kumar <vivek965300@gmail.com>
2026-02-24 10:50:33 +0530

    Login and SignUp Pages added

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth-routes.js";
 import messageRoutes from "./routes/message-routes.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 
@@ -13,6 +14,7 @@ const _dirname = path.resolve();
 const PORT = ENV.PORT || 5000 ;
 
 app.use(e.json()) // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true}));
 app.use(cookieParser());
 
 app.use('/api/auth/',authRoutes);
